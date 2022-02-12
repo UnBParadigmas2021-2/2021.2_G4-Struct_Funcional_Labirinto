3ce8949a85c32c9df61715f49728b6bb8c491f64
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-02-12 15:42:21 -0300

    Trying to create a while loop

--- maze/src/Main.elm
+++ maze/src/Main.elm
@@ -27,73 +27,18 @@ graph =
     , [ 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ]
     ]
 
+while : (Int -> Bool) -> Int -> Program () () () -> Program () () ()
+while condition initialState body =
+  if not (condition initialState) then picture ( 5, 3 ) <| [ rectangle 5 3 single ]
+  else while condition (initialState + 1) body
 
+verify : Int -> Bool
+verify state =
+  state < 1000
 
--- recebe uma lista de inteiros e retorna uma lista de tuplas de valor e indice
+drawMaze =
+  while (verify) 0 (picture ( 5, 3 ) <| [ rectangle 5 3 single ])
 
-
-indexedList : List Int -> List ( Int, Int )
-indexedList list =
-    List.indexedMap Tuple.pair list
-
-
-
--- recebe um indice e uma lista retorna uma lista somente com o indice
-
-
-getByIndex : Int -> List ( Int, Int ) -> List ( Int, Int )
-getByIndex i list =
-    List.filter (\n -> Tuple.first n == i) list
-
-
-
--- produz os shapes da linha horizontal
-
-
-mapRow : Int -> ( Int, Int ) -> List ( Int, Int ) -> Maybe Shape
-mapRow rowIndex elem row =
-    if Tuple.first elem == 0 || Tuple.second elem == 0 then
-        -- ignora o primeiro elemento e os elementos com valor 0
-        Nothing
-
-    else if [ ( Tuple.first elem - 1, 0 ) ] == getByIndex (Tuple.first elem - 1) row then
-        -- ignora se o elemento anterior for zero
-        Nothing
-
-    else if [ ( Tuple.first elem - 1, 2 ) ] == getByIndex (Tuple.first elem - 1) row && Tuple.second elem == 2 then
-        -- faz uma linha dupla
-        Just (move ((Tuple.first elem - 1) * 3) (rowIndex * 2) (line 4 horizontal double))
-
-    else
-        -- faz uma linha simples
-        Just (move ((Tuple.first elem - 1) * 3) (rowIndex * 2) (line 4 horizontal single))
-
-
-generateRow : List Int -> Int -> List Shape
-generateRow row rowIndex =
-    List.filterMap identity <|
-        List.map (\x -> mapRow rowIndex x (indexedList row)) (indexedList row)
-
-
-main : Html.Html msg
+main : Program () () ()
 main =
-    Html.pre []
-        [ Html.text <|
-            render ( 60, 30 )
-                (generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 0
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 1
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 2
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 3
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 4
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 5
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 6
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 7
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 8
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 9
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 10
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 11
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 12
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 13
-                    ++ generateRow [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] 14
-                )
-        ]
+  drawMaze
