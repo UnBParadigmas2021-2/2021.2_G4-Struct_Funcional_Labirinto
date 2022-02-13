863df9e9489c2e573ad94717bb25ed26a6351050
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-02-12 22:58:49 -0300

    Use Matrix library to manipulate graph more freely

--- maze/src/Main.elm
+++ maze/src/Main.elm
@@ -1,21 +1,24 @@
 module Main exposing (main)
 
 import BoxDrawing exposing (..)
+import Matrix exposing (..)
 import Maze exposing (graph)
 
 
 main : Program () () ()
 main =
-    picture ( 50, 50 ) <|
-        List.map
-            (\x ->
-                group
-                    (List.map
-                        (\y ->
-                            rectangle 6 6 single
-                                |> move (Tuple.first y) (Tuple.second y)
-                        )
-                        x
+    picture ( 100, 100 ) <|
+        [ group
+            (List.range 1 (height graph)
+                |> List.concatMap
+                    (\i ->
+                        List.range 1 (width graph)
+                            |> List.concatMap
+                                (\j ->
+                                    [ rectangle 6 6 single
+                                        |> move i j
+                                    ]
+                                )
                     )
             )
-            graph
+        ]
