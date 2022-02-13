0e76f1a43a6faa8889e0fc7d1912f58311aaf80a
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-02-13 10:03:37 -0300

    Use render inside Html instead of picture

--- maze/src/Main.elm
+++ maze/src/Main.elm
@@ -1,12 +1,16 @@
 module Main exposing (main)
 
-import BoxDrawing exposing (group, picture)
+import BoxDrawing exposing (group, render)
+import Html
 import Maze exposing (draw_graph, graph)
 
 
-main : Program () () ()
+main : Html.Html msg
 main =
-    picture ( 100, 100 ) <|
-        [ group
-            (draw_graph graph)
+    Html.pre []
+        [ Html.text <|
+            render ( 100, 100 ) <|
+                [ group
+                    (draw_graph graph)
+                ]
         ]
