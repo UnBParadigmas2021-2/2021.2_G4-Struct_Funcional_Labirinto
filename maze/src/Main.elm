0898758c66b6e2684c5cfa4b8837badee61ecbe1
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-02-12 22:25:50 -0300

    Create 100x100 maze

--- maze/src/Main.elm
+++ maze/src/Main.elm
@@ -1,15 +1,7 @@
 module Main exposing (main)
 
 import BoxDrawing exposing (..)
-
-
-graph : List (List ( number, number ))
-graph =
-    [ [ ( 1, 1 ), ( 1, 2 ), ( 1, 3 ), ( 1, 4 ) ]
-    , [ ( 2, 1 ), ( 2, 2 ), ( 2, 3 ), ( 2, 4 ) ]
-    , [ ( 3, 1 ), ( 3, 2 ), ( 3, 3 ), ( 3, 4 ) ]
-    , [ ( 4, 1 ), ( 4, 2 ), ( 4, 3 ), ( 4, 4 ) ]
-    ]
+import Maze exposing (graph)
 
 
 main : Program () () ()
@@ -20,7 +12,7 @@ main =
                 group
                     (List.map
                         (\y ->
-                            rectangle 1 1 single
+                            rectangle 6 6 single
                                 |> move (Tuple.first y) (Tuple.second y)
                         )
                         x
